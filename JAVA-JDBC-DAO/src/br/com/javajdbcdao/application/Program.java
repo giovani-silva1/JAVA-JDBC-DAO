2bf70095f7c8f9729f08f639bad3b521204ee29d
Giovani Silva <giovani-silva1@live.com>
2021-04-19 15:48:42 -0300

    Finalizado o mapeamento da classe Seller

--- JAVA-JDBC-DAO/src/br/com/javajdbcdao/application/Program.java
+++ JAVA-JDBC-DAO/src/br/com/javajdbcdao/application/Program.java
@@ -1,12 +1,19 @@
 package br.com.javajdbcdao.application;
 
+import java.util.Date;
+
 import br.com.javajdbcdao.entities.Department;
+import br.com.javajdbcdao.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Department department = new Department(1,"books");
+
+		Department department = new Department(1, "books");
+
+		Seller seller = new Seller(1, "Carla", "carla@gmail.com", new Date(), 3000.00, department);
+
 		System.out.println(department);
+		System.out.println(seller);
 	}
 }
