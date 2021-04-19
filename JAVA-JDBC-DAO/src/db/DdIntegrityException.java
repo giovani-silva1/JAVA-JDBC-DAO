4a8c37c5226811c9df06062c7fb3490b9c6f2ded
Giovani Silva <giovani-silva1@live.com>
2021-04-19 15:13:59 -0300

    Adicionado a estrutura do projeto inicial

---
+++ JAVA-JDBC-DAO/src/db/DdIntegrityException.java
@@ -0,0 +1,13 @@
+package db;
+
+public class DdIntegrityException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	public DdIntegrityException(String msg) {
+		super(msg);
+	}
+
+}
